8029ce7757acf5658f202a717fdd8752ba3b84b9
George Ruiz <georuiz817@gmail.com>
2026-05-01 19:10:27 -0400

    previous push was seller dashboard password reset and account and dashboard to be in same style, this one is fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,23 @@
 import type { Metadata } from "next";
+import { Dela_Gothic_One, Nunito } from "next/font/google";
 import { CartProvider } from "@/components/CartProvider";
 import MarqueeBanner from "@/components/MarqueeBanner";
 import Nav from "@/components/Nav";
 import ShowMarqueeOnHome from "@/components/ShowMarqueeOnHome";
 import "@/styles/globals.css";
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  variable: "--font-body",
+  display: "swap",
+});
+
+const delaGothicOne = Dela_Gothic_One({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-heading",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "kuraMart",
@@ -16,7 +30,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${nunito.variable} ${delaGothicOne.variable}`}
+    >
       <body>
         <CartProvider>
           <ShowMarqueeOnHome>
